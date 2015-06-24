7136b4efe7917d0c6755ae4133ebcf9da650b02f
shidaniel <shidanielster@gmail.com>
2015-06-24 12:06:40 +0000

    version 0

---
+++ src/LengthOfLongestSubStringTwoDistinct.java
@@ -0,0 +1,23 @@
+class LengthOfLongestSubStringTwoDistinct{
+    /*
+     * Given a string S, find the length of the longest substring T that 
+     * contains at most two distinct characters
+     */
+    public static void main(String ... args){
+        System.out.println(length("abbac")+ " expected: 4");
+    }
+    
+    
+    /*
+     * first throught: brute force way
+     * Solution: 
+     *     Traverse all the strings searching for substring
+     *     This is a o(n3) approach.
+     * 
+     */
+    
+    public static int length(String s){
+        int max = 0;
+        return max;
+    }
+}
