b1ffb099937ae4122d2528c1883bb3ac736d1eb1
shidaniel <shidanielster@gmail.com>
2015-07-22 12:34:38 +0000

    did some update for better reading

--- src/Zigzag.java
+++ src/Zigzag.java
@@ -4,6 +4,12 @@ public class Zigzag {
      * y = x + 2 (n-i);
      * y = x + 2 (i-1)
      * if n==i==1 return s
+     * steps to prove algorithm converges
+     * y-x= 2(n-i)>=0
+     * y-x= 2(i-1)>=0
+     * 1<= i <= n
+     * Both equals should be there at the same time
+     * this means n>1
      */
      
     public String convert(String s, int numRows) {
@@ -29,4 +35,30 @@ public class Zigzag {
         }
         return sb.toString();
     }
+    
+    /*
+     * did some code merge
+     * use boolean to check the status as most of other code are the same
+     */
+     
+    public String convert2(String s, int numRows) {
+        if(numRows==1)return s;
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i <= numRows;i++){
+            int x = i;
+            if(x<=s.length()){
+                sb.append(s.charAt(x-1));
+            }
+            boolean down = true;
+            while(x<s.length()){
+                int y = down? x + 2*(numRows-i):x+2*(i-1);
+                if(y!=x&&y<=s.length()){
+                    sb.append(s.charAt(y-1));
+                }
+                x=y;
+            }
+        }
+        return sb.toString();
+    }
+    
 }
