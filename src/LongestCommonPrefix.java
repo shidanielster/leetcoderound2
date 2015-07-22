abeaac5f98105ad2e9b4294ed52bc5ec4d6e005a
shidaniel <shidanielster@gmail.com>
2015-07-22 12:48:35 +0000

    0722

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+class LongestCommonPrefix{
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length<1)return "";
+        Arrays.sort(strs);
+        
+        int i = 0;
+        
+        String a = strs[0];
+        String b = strs[strs.length-1];
+        
+        while(i<a.length()&&i<b.length()&&a.charAt(i)==b.charAt(i))i++;
+        return a.substring(0,i);
+    }
+}
