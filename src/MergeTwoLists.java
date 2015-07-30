afe782ecc60e3d11e0880d2cfbb837eb7ff7706b
shidaniel <shidanielster@gmail.com>
2015-07-30 00:53:20 +0000

    0728

--- src/MergeTwoLists.java
+++ src/MergeTwoLists.java
@@ -21,4 +21,32 @@ class  MergeTwoLists{
         s.next = l1==null?l2:l1;
         return head.next;
     }
+    
+    /*
+     * This urgly solution is 10% fast than previous
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode s = new ListNode(0);
+        ListNode head = s;
+        if(l1==null)return l2;
+        else if (l2==null)return l1;
+        
+        while(true){
+            if(l1.val<l2.val){
+                s.next=l1;
+                l1=l1.next;
+                s=s.next;
+
+                if(l1==null)break;
+                
+            }else{
+                s.next=l2;
+                s=s.next;
+                l2=l2.next;
+                if(l2==null)break;
+            }
+        }
+        s.next = l1==null?l2:l1;
+        return head.next;
+    }
 }
