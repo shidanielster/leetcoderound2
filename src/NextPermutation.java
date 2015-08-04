9d28962fafe5115a0e9e884d03e9230d3d1d9219
shidaniel <shidanielster@gmail.com>
2015-08-04 03:19:20 +0000

    0804

---
+++ src/NextPermutation.java
@@ -0,0 +1,36 @@
+class NextPermutation{
+    public void nextPermutation(int[] nums) {
+        int x = nums.length-1;
+        int y = nums.length -1;
+        
+        //find x  x<x++
+        //not found, return last
+        for(int i = nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                x= i;
+                break;
+            }
+        }
+        if(x==nums.length-1){
+            return reverse(nums,0,nums.length-1);
+        }
+        for(int i=x+1;i<nums.length-;i++){
+            if(nums[i]>nums[x]&&nums[i+1]<nums[x]){
+                y=i;
+                break;
+            }
+        }
+        int t = nums[x];
+        nums[x]=nums[y];
+        nums[y]= t;
+        
+        reverse(nums,x+1,nums.length-1);
+    }
+    private void reverse(int[] n,int x, int y){
+        while(x<y){
+            int t = nums[x];
+            nums[x]=nums[y];
+            nums[y]= t;
+        } 
+    }
+}
