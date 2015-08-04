000314f3946780460a40e5203d7f02ec73c1b65e
shidaniel <shidanielster@gmail.com>
2015-08-04 15:40:58 +0000

    0804

---
+++ src/Divide.java
@@ -0,0 +1,42 @@
+public class Divide {
+
+    /*
+     * simple thought
+     * each int could be represented as 2N+2(n-1)+...+2+2(0)
+     * so use 1<<x should be fine
+     * others will need to check corner cases
+     */
+    
+    public int divide(int d1, int d2) {
+        if(d1==(1<<31)){
+            if(d2==-1){return divide(d1-d2,d2);}
+            else{ return d2>0?divide(d1+d2,d2)-1: divide(d1-d2,d2)+1; }       
+        }else if(d2==1<<31){
+        	return 0;
+        }
+        if((d1^d2)<0){
+            int x = divide(Math.abs(d1),Math.abs(d2));
+            return -x;
+        }else if(d1<0){
+        	return divide(Math.abs(d1),Math.abs(d2));
+        }
+        if(d1<d2)return 0; 
+        
+        int x = 0;
+        for(int i = 0;i<31;i++){
+            if(d2<<i <= d1&&d2<<i+1>d1 || d2<<i <= d1 && d2<<i+1 < 0){
+                x = i;
+                break;
+            }
+        }
+        int ret = 1<<x;
+        d1-=d2<<x;
+        for(int i=x-1;i>=0;i--){
+            if(d2<<i <=d1){
+                ret += 1<<i;
+                d1-=d2<<i;
+            }
+        }
+        return ret;
+    }
+}
