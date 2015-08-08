0dfd559832e86e803567c7557431d8aa77ce3089
shidaniel <shidanielster@gmail.com>
2015-08-08 13:34:02 +0000

    0808

--- src/NextPermutation.java
+++ src/NextPermutation.java
@@ -2,41 +2,30 @@ class NextPermutation{
     /*
      * This is a classic algorithm to use Lexicographic method to generate full array
      */
-    public void nextPermutation(int[] nums) {
-        int x = nums.length-1;
-        int y = nums.length -1;
-        
-        //find x  x<x++
-        //not found, return last
-        for(int i = nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                x= i;
-                break;
-            }
-        }
-        if(x==nums.length-1){
+ public void nextPermutation(int[] nums) {
+        if(nums.length<2)return ;
+        int i = nums.length-2;
+        while(i>0&&nums[i]>=nums[i+1])i--;
+        int j = i;
+        while(j<nums.length-1&&nums[j+1]>nums[i])j++;
+        if(nums[i]==nums[j]){
             reverse(nums,0,nums.length-1);
-            
-        }else{
-            for(int i=x+1;i<nums.length-1;i++){
-                if(nums[i]>nums[x]&&nums[i+1]<=nums[x]){
-                    y=i;
-                    break;
-                }
-            }
-            int t = nums[x];
-            nums[x]=nums[y];
-            nums[y]= t;
-        
-            reverse(nums,x+1,nums.length-1);
+            return ;
         }
+        swap(nums,i,j);
+        reverse(nums,i+1,nums.length-1);
     }
-    private void reverse(int[] n,int x, int y){
-        while(x<y){
-            int t = n[x];
-            n[x]=n[y];
-            n[y]= t;
-            x++;y--;
-        } 
+    
+    private void swap(int[] nums,int i,int j){
+        int t = nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    
+    private void reverse(int[] nums,int i,int j){
+        while(i<j){
+            swap(nums,i,j);
+            i++;j--;
+        }
     }
 }
