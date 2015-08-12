2862e653d307fcb300a6697d8c24e792ff50ff9d
shidaniel <shidanielster@gmail.com>
2015-08-12 23:10:55 +0000

    0813

--- src/SearchInsert.java
+++ src/SearchInsert.java
@@ -14,4 +14,27 @@ class SearchInsert{
         }
         return nums[s]<target?s+1:s;
     }
+    
+    
+    /* 
+     * A fast solution
+     * This solution is to search x, which x<target<x+1
+     * Then the position returned should be x+1
+     */
+    public int searchInsert2(int[] nums, int target) {
+        int s = 0;
+        //init to length
+        //as s<=e-1, so s can be [0,n-1]
+        int e = nums.length;
+        while(s+1!=e){
+            int m = (s+e)/2;
+            // assume s<=target<e
+            if(target<nums[m]){
+                e=m;
+            }else {
+                s=m;
+            }
+        }
+        return nums[s]<target?s+1:s;
+    }
 }
